2346ab4863346af6f9af78fe284371fde586d5ae
jebeld17 <jebeld17@gmail.com>
2020-02-29 15:08:33 -0500

    Created EXE from BAT, then moved origial BAT file to its own folder

--- InstallWSL.bat
+++
@@ -1,1 +0,0 @@
-powershell -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/k PowerShell Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux && shutdown -r && exit'"
